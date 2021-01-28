950e2fdb3188d731b3ea2d695cad483858048d01
hieule247 <54555669+hieule247@users.noreply.github.com>
2021-01-27 19:51:22 -0600

    Add files via upload

---
+++ Labs/week3_day02_Step2_DBPrototype/AcmApp/app1.js
@@ -0,0 +1,29 @@
+// declare using mongoose
+// require("./api/data/db.js");
+var express = require("express");
+var path = require("path");
+// var routes = require("./api/routes");
+var bodyParser = require("body-parser");
+
+const app = express();
+app.set("port", 3000);
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+// app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+// app.use("/api", routes);
+
+
+const server = app.listen(app.get("port"), function(){
+    var port = server.address().port;
+    console.log("Listening to port ..." + port);
+});
