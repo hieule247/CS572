3f747690d23605213a1e2a8350ee1edecda1b9ac
hieule247 <54555669+hieule247@users.noreply.github.com>
2021-02-06 20:37:14 -0600

    Delete Labs/week4_day02_Angular/AcmApp directory

--- Labs/week4_day02_Angular/AcmApp/api/data/course-model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var courseSchema = new mongoose.Schema({
-    name: String,
-    notes: String
-});
-
-// model
-module.exports = mongoose.model("Course", courseSchema, "courses");
